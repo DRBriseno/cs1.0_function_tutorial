7f060d0a6af6a3872686dc4ac0af8501326fe197
Rose <71735227+DRBriseno@users.noreply.github.com>
2020-11-12 11:48:33 -0800

    Add files via upload
    This is my function tutorial for CS.

---
+++ CS_FunctionTutorial.py
@@ -0,0 +1,53 @@
+# This is my function tutorial.
+
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print("the area of 5 and 6 is:",result)
+
+result = area(7,9)
+print("the area of 7 and 9 is:",result)
+
+result = area(4,8)
+print("the area of 4 and 8 is:",result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(5,6)
+print("the difference of 5 and 6 is:",result)
+
+result = subtract(7,9)
+print("the difference of 7 and 9 is:",result)
+
+result = subtract(4,8)
+print("the difference of 4 and 8 is:",result)
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+result = divide(50,5)
+print("the quotient of 50 and 5 is:",result)
+
+result = divide(90,9)
+print("the quotient of 90 and 9 is:",result)
+
+result = divide(40,8)
+print("the quotient of 40 and 8 is:",result)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = add(50,5)
+print("the sum of 50 and 5 is:",result)
+
+result = add(90,9)
+print("the sum of 90 and 9 is:",result)
+
+result = add(40,8)
+print("the sum of 40 and 8 is:",result)
